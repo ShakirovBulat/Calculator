4e7503a2f0687f6159c33f49b6b751103abe1ee3
булат шакиров <a2009-1@mail.ru>
2021-06-25 17:04:47 +0300

    vvod procok

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -23,12 +23,15 @@ namespace Calculator
             string ProcD = Convert.ToString(textBox3);
             int kolD = Convert.ToInt32(textBox4.Text);
             double[] razd = textBox3.Text.Split(' ').Select(x => double.Parse(x)).ToArray();
-
-            foreach(var Sum in razd)
+            var r = ProcD.Where(x => char.IsDigit(x)).Sum(x => char.GetNumericValue(x));
+            r = r / 10 / kolD;
+            textBox6.Text = Convert.ToString(r);
+            foreach (var Sum in razd)
             {
-                textBox1.Text = Convert.ToString((SumR / 100) * 0.9 * kolD + SumR);
+                textBox1.Text = Convert.ToString((SumR / 100) * r * kolD + SumR);
+                textBox5.Text = Convert.ToString(((SumR / 100) * r * kolD + SumR) - SumR);
             }
-            textBox5.Text = Convert.ToString(((SumR / 100) * 0.9 * kolD + SumR) - SumR);
+            
         }
     }
 }
