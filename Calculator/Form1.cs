f828e8b8b04b6c74e91e7b9832c752d62989b36a
булат шакиров <a2009-1@mail.ru>
2021-06-25 15:09:01 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int SumR = Convert.ToInt32(textBox1);
+            int ProcD = Convert.ToInt32(textBox3);
+            int kolD = Convert.ToInt32(textBox2);
+            int ObSum = 0;
+
+            for (int i = 1; i <= kolD; i++)
+            {
+                ObSum = (SumR / 100) * ProcD;
+            }
+        }
+    }
+}
